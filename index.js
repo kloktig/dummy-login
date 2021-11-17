353d490f41676f29affb223ef59a6a61c0cba556
Roger Hoem-Martinsen <roger.h.martinsen@gmail.com>
2021-11-17 22:08:00 +0100

    separate node index.js from k6

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 import {LoginClient} from "./src/loginClient.js";
-import {LoginClientK6} from "./src/loginClientk6.js"
 import {parseJwt} from "./src/parseJwt.js";
-import {parseJwtK6} from "./src/parseJwtK6.js";
 
-export {parseJwt, parseJwtK6, LoginClient, LoginClientK6}
+export {parseJwt, LoginClient}
---
+++ indexk6.js
@@ -0,0 +1,4 @@
+import {LoginClientK6} from "./src/loginClientk6.js"
+import {parseJwtK6} from "./src/parseJwtK6.js";
+
+export {parseJwtK6, LoginClientK6}
